8651e124b005d15b09250e515f9cc62a16e61b7b
mateus schmitz <mateus.schmitz7@hotmail.com>
2023-09-29 12:52:10 -0300

    feat: create new register button

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,8 @@ import prisma from 'lib/prisma';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 // ** Demo Components Imports
 import UsersTable from 'src/views/users/List';
@@ -15,8 +17,8 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const users = await prisma.user.findMany();
 
   return {
-    props: { 
-      users : JSON.parse(JSON.stringify(users))
+    props: {
+      users: JSON.parse(JSON.stringify(users))
     }
   };
 };
@@ -29,9 +31,14 @@ const UsersList: React.FC<Props> = (props) => {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
+        <Box sx={{ textAlign: 'right', padding: '10px' }}>
+          <Button variant='contained' color='primary' href='/users/create'>
+            Novo Usuário
+          </Button>
+        </Box>
         <Card>
           <CardHeader title='Usuários' titleTypographyProps={{ variant: 'h6' }} />
-          <UsersTable users={props.users}/>
+          <UsersTable users={props.users} />
         </Card>
       </Grid>
     </Grid>
